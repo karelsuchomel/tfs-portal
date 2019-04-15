75dff5f8bcc8944ca24bc882ffe07eddf9d317ed
Karel Suchomel <karelsuchomel@windowslive.com>
2019-04-15 18:18:39 +0200

    REWORKING header 20%

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,30 +1,24 @@
 import React from 'react';
 
+import MobileNavigationButton from './MobileNavigationButton.js'
 import SiteIdentity from './SiteIdentity.js';
 import Search from './Search.js';
-import FocusedLinks from './FocusedLinks.js';
+import QuickLinksNavigation from './QuickLinksNavigation.js';
 import UserNavigation from './UserNavigation.js'
 
 export default class NavigationTopBar extends React.Component {
 	render() {
 		return(
 			<nav id="header">
-				<input name="toggle-menu" id="toggle-menu-checkbox" type="checkbox" />
 				<div id="top-bar-wrapper" className="clear-both">
-					<label htmlFor="toggle-menu-checkbox" className="icon-hamburger-menu">
-						<div className="menu-icon-container">
-							<div id="hamburger-line-1" className="line"></div>
-							<div id="hamburger-line-2" className="line"></div>
-							<div id="hamburger-line-3" className="line"></div>
-						</div>
-						<span>Navigace</span>
-					</label>
+					
+					<MobileNavigationButton />
 
 					<SiteIdentity />
 
 					<Search />
 
-					<FocusedLinks />
+					<QuickLinksNavigation />
 
 					<UserNavigation />
 					
---
+++ src/components/header/MobileNavigationButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MobileNavigationButton = () =>
+	<div id="mobile-navigation-button">
+		<div className="menu-icon-container" tabindex="0">
+			<div id="hamburger-line-1" className="line"></div>
+			<div id="hamburger-line-2" className="line"></div>
+			<div id="hamburger-line-3" className="line"></div>
+		</div>
+	</div>
+
+export default MobileNavigationButton
